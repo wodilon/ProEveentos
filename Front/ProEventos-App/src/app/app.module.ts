d6815f67daa32d554bd2319eb923d45c36b52c79
wodilon <wendy.odilon@hotmail.com>
2022-06-01 11:05:13 -0400

    cords master

---
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventosComponent,
+      PalestrantesComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot()
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+public eventos: any = [
+
+]
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+    // this.eventos = [
+    //   {
+    //     Tema: 'Angular 11',
+    //     Local: 'Belo Horizonte'
+    //   },
+    //   {
+    //     Tema: '.NET 5',
+    //     Local: 'São Paulo'
+    //   },
+    //   {
+    //     Tema: 'Angular e Suas Novidades',
+    //     Local: 'Rio de Janeiro'
+    //   }
+    // ];
+  }
+  
+}
